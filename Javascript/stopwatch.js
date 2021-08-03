62e3c54f22e7da1e9076fd66fe5f427dfa16d103
Sheyla <shel.busta@gmail.com>
2021-08-03 17:03:04 +0200

    stopwatch

--- Javascript/stopwatch.js
+++ Javascript/stopwatch.js
@@ -1,25 +1,72 @@
-const milliseconds = document.getElementById("milliseconds");
-const seconds = document.getElementById("seconds");
-const minutes = document.getElementById("minutes");
-const hours = document.getElementById("hours");
+let startstop = 0;
 
-// const countDown = () => {
-//   const countDate = new Date("Jul 29, 2021 00:00:00").getTime();
-//   //   console.log(countDate);
-//   const now = new Date().getTime();
-//   const gap = countDate - now;
-//   //   console.log(gap);
+const startStop = () => {
+  startstop = startstop + 1;
+  if (startstop === 1) {
+    startTimer();
+    document.getElementById("btn-toggle").innerHTML = "Stop";
+  } else if (startstop === 2) {
+    document.getElementById("btn-toggle").innerHTML = "Start";
+    startstop = 0;
+    stopTimer();
+  }
+};
 
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
+let ms = 0;
+let sec = 0;
+let min = 0;
+let hr = 0;
 
-//   // calculate
+//display
 
-//   const textDay = Math.floor(gap / day);
-//   console.log(textDay);
-// };
+const timer = () => {
+  ms++;
+  if (ms >= 100) {
+    sec++;
+    ms = 0;
+  }
+  if (sec === 60) {
+    min++;
+    sec = 0;
+  }
+
+  if (min === 60) {
+    hour++;
+    min = 0;
+  }
+  if (hr === 24) {
+    ms, sec, min, (hr = 0);
+  }
+
+  document.getElementById("milliseconds").innerHTML = ms;
+  document.getElementById("seconds").innerHTML = sec;
+  document.getElementById("minutes").innerHTML = min;
+  document.getElementById("hours").innerHTML = hr;
+  // document.getElementById("seconds").innerText = displaysec,
+};
+
+let time = 0;
+
+const startTimer = () => {
+  time = setInterval(timer, 10);
+};
+
+const stopTimer = () => {
+  clearInterval(time);
+};
+
+const reset = () => {
+  ms = 0;
+  sec = 0;
+  min = 0;
+  hr = 0;
+};
 
 const startWatch = document.getElementById("btn-toggle");
-startWatch.addEventListener("click", countDown);
+startWatch.addEventListener("click", startTimer);
+
+//const stopWatch = document.getElementById("btn-stop");
+//stopWatch.addEventListener("click", stopTimer, false);
+
+const resetWatch = document.getElementById("btn-reset");
+resetWatch.addEventListener("click", reset);
